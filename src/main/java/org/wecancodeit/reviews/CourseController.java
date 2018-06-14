9ec2eeb02f7265d68d6c990fedfc2449411787c4
JasmineCarter <jlcart6@gmail.com>
2018-06-14 19:09:40 -0400

    almost done with part 3

--- src/main/java/org/wecancodeit/reviews/CourseController.java
+++ src/main/java/org/wecancodeit/reviews/CourseController.java
@@ -1,14 +1,26 @@
 package org.wecancodeit.reviews;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CourseController {
 
-	
+@Resource
+ReviewRepository reviewRepo; 
+
 	@RequestMapping("/show-reviews")
-	public String findAllReviews() {
+	public String findAllReviews(Model model) {
+		model.addAttribute("reviews", reviewRepo.findAll()); 
 		return "reviews"; 
+	}
+	
+	@RequestMapping("/review")
+	public String findOneCourse(@RequestParam(value="id")Long id) {
+		return ""; 
 	}
 }
--- src/test/java/org/wecancodeit/reviews/ReviewControllerMockMvcTest.java
+++ src/test/java/org/wecancodeit/reviews/ReviewControllerMockMvcTest.java
@@ -2,17 +2,28 @@ package org.wecancodeit.reviews;
 
 import javax.annotation.Resource;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvc; 
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get; 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status; 
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.util.Collection;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status; 
+import static java.util.Arrays.asList; 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model; 
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CourseController.class)
@@ -22,8 +33,35 @@ public class ReviewControllerMockMvcTest {
 	@Resource
 	private MockMvc mvc; 
 	
+	@Mock
+	private Review firstReview; 
+	
+	@Mock Review secondReview; 
+	
+	@MockBean
+	private ReviewRepository repository; 
+	
 	@Test
 	public void shouldBeOkForAllReviews() throws Exception {
 		mvc.perform(get("/show-reviews")).andExpect(status().isOk()); 
 }
+	
+	@Test
+	public void shouldRouteToAllReviewsView() throws Exception {
+		mvc.perform(get("/show-reviews")).andExpect(view().name(is("reviews"))); 
+	}
+	
+	@Test
+	public void shouldPutAllReviewsIntoModel() throws Exception {
+		Collection<Review> allReviews = asList(firstReview, secondReview); 
+		when(repository.findAll()).thenReturn(allReviews); 
+		mvc.perform(get("/show-reviews")).andExpect(model().attribute("reviews", is(allReviews))); 
+	}
+	
+	@Test
+	public void shouldBeOkForASingleReview() throws Exception {
+		mvc.perform(get("/show-review?id=1")).andExpect(status().isOk()); 
+	}
+	
+	
 }
