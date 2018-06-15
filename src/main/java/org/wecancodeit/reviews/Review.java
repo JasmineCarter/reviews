a485550a5617f1a6a17322511595ff666029c54e
JasmineCarter <jlcart6@gmail.com>
2018-06-14 23:24:09 -0400

    just need imgs

--- src/main/java/org/wecancodeit/reviews/Review.java
+++ src/main/java/org/wecancodeit/reviews/Review.java
@@ -1,16 +1,28 @@
 package org.wecancodeit.reviews;
 
 public class Review {
-
+ 
 	private long reviewId;
+	private String name; 
+	private String description; 
 
-	public Review(long reviewId, String string, String string2) {
-		this.reviewId = reviewId; 
+	public Review(long reviewId, String name, String description) {
+		this.reviewId = reviewId;
+		this.name = name; 
+		this.description = description; 
 	
 	}
 
 	public Long getId() {
 		return reviewId;
 	}
+	
+	public String getName() {
+		return name; 
+	}
 
+	
+	public String getDescription() {
+		return description; 
+	}
 }
